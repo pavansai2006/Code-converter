4ac9a4d7b766f395ac73338ffadc11e3609e8642
Pavan <144151465+pavansai2006@users.noreply.github.com>
2025-05-15 19:36:27 +0530

    Create design.sv

---
+++ design.sv
@@ -0,0 +1,12 @@
+module comparator(B2,B1,B0,G2,G1,G0);
+  input B2,B1,B0;
+  output reg G2,G1,G0;
+  
+  always @(*)
+    begin
+      G2=0; G1=0; G0=0;
+      G0 = B1 ^ B0;
+      G1 = B2 ^ B1;
+      G2 = B2;
+    end
+endmodule
